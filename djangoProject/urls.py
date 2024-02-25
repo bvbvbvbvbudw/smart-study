dd7bdc4f81319940035def223b5ec945f52e13f6
Bogdan Bikbulatov <batmansila1234@gmail.com>
2024-02-25 15:43:53 +0200

    add auth

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authApp.urls')),
     path('', include('frontSmart.urls')),
 ]
 
