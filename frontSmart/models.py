ba101b8b3dae831074f804110b04b674fb51bcae
Isaienko Volodymyr <isaienkovova05@gmail.com>
2024-03-15 08:57:13 +0200

    updated main

--- frontSmart/models.py
+++ frontSmart/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
 
 class Task(models.Model):
     subject = models.CharField(max_length=255)
@@ -9,5 +8,3 @@ class Task(models.Model):
     task = models.CharField(max_length=255)
     topic = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-=======
->>>>>>> 09c94878ff8f9f97a241ef29a5cbfd6a72af3c3c
